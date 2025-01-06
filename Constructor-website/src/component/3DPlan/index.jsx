053dae01e07e170729a1d8446947b00f9d9c0587
Pooja-Mehra9921 <159164676+Pooja-Mehra9921@users.noreply.github.com>
2025-01-06 23:11:14 +0530

    update code

--- Constructor-website/src/component/3DPlan/index.jsx
+++ Constructor-website/src/component/3DPlan/index.jsx
@@ -1,9 +1,16 @@
+import { Typography } from "@mui/material";
 import React from "react";
+import Carousel from "../../component/Carousel"
 
 const ThreeDPlan =()=>{
     return(
         <>
+        <Typography>
+        <h1>3D Plan</h1>
+<Carousel/>
+        </Typography>
         This is a 3d plan component
+        
         </>
     )
 };
